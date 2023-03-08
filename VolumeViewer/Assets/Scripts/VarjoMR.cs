18f9145251ad5fa79ba126d1f8856f75bb0a7023
davidaigner98 <davidaigner98@gmail.com>
2023-03-08 15:31:26 +0100

    Setup Modifications for AR
    Player Settings modified and created a script in order to support Varjo.

---
+++ VolumeViewer/Assets/Scripts/VarjoMR.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Varjo.XR;
+
+public class VarjoMR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        VarjoMixedReality.StartRender();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
