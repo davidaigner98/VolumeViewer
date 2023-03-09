761bb2ec4b0c97d8ccdf4fa89b9f729a6b4cb961
DavidAigner98 <davidaigner98@gmail.com>
2023-03-09 15:56:12 +0100

    Added configurable offset to AlignToTransform-Script
    Added a Vector3 public attribute to the AlignToTransform-Script, in order to make the offset to the transform configurable.

--- VolumeViewer/Assets/Scripts/AlighToTransform.cs
+++ VolumeViewer/Assets/Scripts/AlighToTransform.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class AlighToTransform : MonoBehaviour {
     public Transform alignTo;
-    
+    public Vector3 offset;
+
     // Start is called before the first frame update
     void Start() {
         transform.eulerAngles = new Vector3(0, alignTo.eulerAngles.y, 0);
-        transform.position = alignTo.position + alignTo.TransformDirection(new Vector3(0, 0, -2));
+        transform.position = alignTo.position + alignTo.TransformDirection(offset);
     }
 }
