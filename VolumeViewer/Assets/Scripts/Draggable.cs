70baa3ad04f996355002591cd04f30c3f972e467
DavidAigner98 <davidaigner98@gmail.com>
2023-03-10 10:45:06 +0100

    Draggable orientation changed to local space of camera
    Draggable now orients itself along the local orientation of the display camera

--- VolumeViewer/Assets/Scripts/Draggable.cs
+++ VolumeViewer/Assets/Scripts/Draggable.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public class Draggable : MonoBehaviour {
+    public Transform displayCameraTransform;
     public float rotSpeed = 250.0f;
 
     // Start is called before the first frame update
@@ -12,7 +13,7 @@ public class Draggable : MonoBehaviour {
         float rotX = Input.GetAxis("Mouse X") * Mathf.Deg2Rad * rotSpeed;
         float rotY = Input.GetAxis("Mouse Y") * Mathf.Deg2Rad * rotSpeed;
 
-        transform.Rotate(Vector3.up, -rotX, Space.World);
-        transform.Rotate(Vector3.right, rotY, Space.World);
+        transform.Rotate(displayCameraTransform.TransformDirection(Vector3.up), -rotX, Space.World);
+        transform.Rotate(displayCameraTransform.TransformDirection(Vector3.right), rotY, Space.World);
     }
 }
