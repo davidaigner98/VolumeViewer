98f9ddd335fd117318189a50dfa152f04e87e5b0
DavidAigner98 <davidaigner98@gmail.com>
2023-03-13 19:31:30 +0100

    Added ExtractModelFromDisplay Method
    Added ExtractModelFromDisplay method to the ModelTransformator script

--- VolumeViewer/Assets/Scripts/ModelTransformator.cs
+++ VolumeViewer/Assets/Scripts/ModelTransformator.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ModelTransformator : MonoBehaviour
-{
+public class ModelTransformator : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
+    }
+
+    public void ExtractModelFromDisplay(float distance) {
+        if (distance < 0) { distance = 0; }
+        else if (distance > 1) { distance = 1; }
+
+        if (distance > 0.05) {
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+
+        transform.localPosition = new Vector3(0, 0, -distance);
+        transform.localScale = new Vector3(1, 1, distance);
     }
 }
