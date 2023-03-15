b1a64a5dd12dff16689cf0b564382d8f8165641d
DavidAigner98 <davidaigner98@gmail.com>
2023-03-15 16:56:46 +0100

    ModelSynchronizer now extends NetworkBehaviour

--- VolumeViewer/Assets/Scripts/ModelSynchronizer.cs
+++ VolumeViewer/Assets/Scripts/ModelSynchronizer.cs
@@ -1,9 +1,10 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class ModelSynchronizer : MonoBehaviour {
+public class ModelSynchronizer : NetworkBehaviour {
     [ServerRpc(RequireOwnership = false)]
     public void RotateModelServerRpc(Vector3 axis, float angle) {
         transform.Rotate(axis, angle);
+        Debug.Log("Server RPC has been called by client!");
     }
 }
