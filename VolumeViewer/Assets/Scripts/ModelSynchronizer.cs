bd2690edbbbcbc0b20eda3bb11e132bd9de9e0af
DavidAigner98 <davidaigner98@gmail.com>
2023-03-15 17:40:21 +0100

    Set Space to World-Space in Rotate method of ModelSynchronizer

--- VolumeViewer/Assets/Scripts/ModelSynchronizer.cs
+++ VolumeViewer/Assets/Scripts/ModelSynchronizer.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class ModelSynchronizer : NetworkBehaviour {
     [ServerRpc(RequireOwnership = false)]
     public void RotateModelServerRpc(Vector3 axis, float angle) {
-        transform.Rotate(axis, angle);
-        Debug.Log("Server RPC has been called by client!");
+        transform.Rotate(axis, angle, Space.World);
     }
 }
