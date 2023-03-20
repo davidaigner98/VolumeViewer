ab9e42448d4af4430df8e48a320ef1eed77c881c
DavidAigner98 <davidaigner98@gmail.com>
2023-03-20 12:17:03 +0100

    Bugfix: destroying Model on accident

--- VolumeViewer/Assets/Scripts/ModelSynchronizer.cs
+++ VolumeViewer/Assets/Scripts/ModelSynchronizer.cs
@@ -30,13 +30,14 @@ public class ModelSynchronizer : NetworkBehaviour {
         transformator.SetAttachedState(attached);
 
         if (attached) {
-            GameObject modelParent = transform.parent.gameObject;
-            transform.SetParent(displayCenter.transform);
-            Destroy(modelParent);
-        } else {
             GameObject modelParent = new GameObject("ModelParent");
             modelParent.transform.rotation = displayCenter.transform.rotation;
             transform.SetParent(modelParent.transform);
+        }
+        else {
+            GameObject modelParent = GameObject.Find("ModelParent");
+            transform.SetParent(displayCenter.transform);
+            Destroy(modelParent);
         }
     }
 
