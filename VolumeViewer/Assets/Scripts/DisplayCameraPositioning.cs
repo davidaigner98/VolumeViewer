09a7c039af0bbe73bcf262100afc84b28df28d0a
DavidAigner98 <davidaigner98@gmail.com>
2023-03-30 14:51:23 +0200

    New formula for 3D projection on display
    Added grid on server

--- VolumeViewer/Assets/Scripts/DisplayCameraPositioning.cs
+++ VolumeViewer/Assets/Scripts/DisplayCameraPositioning.cs
@@ -1,17 +1,78 @@
+using System.Net.Security;
 using UnityEngine;
 
 public class DisplayCameraPositioning : MonoBehaviour {
     public static DisplayCameraPositioning Instance { get; private set; }
+    public Vector3 cageSize;
+    public bool drawCage;
     private Vector3 startPosition;
 
     private void Awake() {
         if (Instance != null && Instance != this) { Destroy(this); }
         else { Instance = this; }
-
-        startPosition = transform.position;
     }
 
-    public void SynchronizeDisplayCameraPosition(Vector3 offset) {
-        transform.position = startPosition + offset;
+    private void Start() {
+        startPosition = transform.position;
+
+        if (drawCage) { DrawCage(); }
+    }
+
+    public void SynchronizeDisplayCameraPosition(Vector3 realPosition) {
+        Debug.Log("RealPosition: " + realPosition);
+        //if (realPosition.z < 0) { return; }
+
+        realPosition += startPosition;
+        realPosition /= realPosition.z;
+        realPosition *= startPosition.z;
+        Debug.Log("Calculated: "+realPosition);
+        //transform.position = realPosition;
+    }
+
+    private void DrawCage() {
+        GameObject cageGO = new GameObject("Grid");
+
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, -cageSize.z), startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, -cageSize.z)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, -cageSize.z), startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, -cageSize.z)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, -cageSize.z), startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, -cageSize.z)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, -cageSize.z), startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, -cageSize.z)).transform.SetParent(cageGO.transform);
+
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 2 / 3), startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 2 / 3)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, -cageSize.z * 2 / 3), startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, -cageSize.z * 2 / 3)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 2 / 3), startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, -cageSize.z * 2 / 3)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 2 / 3), startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, -cageSize.z * 2 / 3 )).transform.SetParent(cageGO.transform);
+
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 1 / 3), startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 1 / 3)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, -cageSize.z * 1 / 3), startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, -cageSize.z * 1 / 3)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 1 / 3), startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, -cageSize.z * 1 / 3)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, -cageSize.z * 1 / 3), startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, -cageSize.z * 1 / 3)).transform.SetParent(cageGO.transform);
+
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, -cageSize.z), startPosition + new Vector3(-cageSize.x / 2, -cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, -cageSize.z), startPosition + new Vector3(-cageSize.x / 2, cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, -cageSize.z), startPosition + new Vector3(cageSize.x / 2, -cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, -cageSize.z), startPosition + new Vector3(cageSize.x / 2, cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+
+        DrawLine(startPosition + new Vector3(-cageSize.x / 2, 0, -cageSize.z), startPosition + new Vector3(-cageSize.x / 2, 0, 0)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 2, 0, -cageSize.z), startPosition + new Vector3(cageSize.x / 2, 0, 0)).transform.SetParent(cageGO.transform);
+
+        DrawLine(startPosition + new Vector3(-cageSize.x / 6, -cageSize.y / 2, -cageSize.z), startPosition + new Vector3(-cageSize.x / 6, -cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(-cageSize.x / 6, cageSize.y / 2, -cageSize.z), startPosition + new Vector3(-cageSize.x / 6, cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 6, -cageSize.y / 2, -cageSize.z), startPosition + new Vector3(cageSize.x / 6, -cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+        DrawLine(startPosition + new Vector3(cageSize.x / 6, cageSize.y / 2, -cageSize.z), startPosition + new Vector3(cageSize.x / 6, cageSize.y / 2, 0)).transform.SetParent(cageGO.transform);
+    }
+
+    private GameObject DrawLine(Vector3 from, Vector3 to) {
+        GameObject newLineGO = new GameObject("Line");
+        LineRenderer renderer = newLineGO.AddComponent<LineRenderer>();
+        renderer.SetPosition(0, from);
+        renderer.SetPosition(1, to);
+        renderer.startWidth = 0.01f;
+        renderer.endWidth = 0.01f;
+
+        Material mat = new Material(Shader.Find("Transparent/Diffuse"));
+        mat.color = new Color(0.2f, 0.2f, 0.2f, 0.5f);
+        renderer.material = mat;
+
+        return newLineGO;
     }
 }
--- VolumeViewer/Assets/Scripts/DisplayProjectionViewTracking.cs
+++ VolumeViewer/Assets/Scripts/DisplayProjectionViewTracking.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class DisplayProjectionViewTracking : MonoBehaviour {
     public GameObject xrCamera;
+    private int frame = 0;
 
     void Update() {
         if (!CrossPlatformMediator.Instance.isServer && !CrossPlatformMediator.Instance.isInLobby) {
-            GameObject displayCenter = DisplayProfileManager.Instance.GetCurrentDisplayCenter();
-            Vector3 cameraOffset = xrCamera.transform.position - displayCenter.transform.position;
-            cameraOffset = displayCenter.transform.TransformDirection(cameraOffset);
+            if (frame >= 50) {
+                GameObject displayCenter = DisplayProfileManager.Instance.GetCurrentDisplayCenter();
+                Vector3 cameraOffset = xrCamera.transform.position - displayCenter.transform.position;
+                cameraOffset = displayCenter.transform.TransformDirection(cameraOffset);
 
-            CrossPlatformMediator.Instance.SynchronizeCameraPositionServerRpc(cameraOffset);
+                CrossPlatformMediator.Instance.SynchronizeCameraPositionServerRpc(cameraOffset);
+                frame = 0;
+            }
+
+            frame++;
         }
     }
 }
