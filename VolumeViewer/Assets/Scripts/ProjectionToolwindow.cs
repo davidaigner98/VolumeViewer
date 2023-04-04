db966fd874aa3b320a38172571d467c40d942da1
davidaigner98 <davidaigner98@gmail.com>
2023-04-04 19:09:05 +0200

    Realigned the ProjectionToolwindow

--- VolumeViewer/Assets/Scripts/ProjectionToolwindow.cs
+++ VolumeViewer/Assets/Scripts/ProjectionToolwindow.cs
@@ -19,12 +19,13 @@ public class ProjectionToolwindow : MonoBehaviour {
         GameObject currCenter = DisplayProfileManager.Instance.GetCurrentDisplayCenter();
         GameObject currSize = DisplayProfileManager.Instance.GetCurrentDisplaySize();
         Vector2 ownSize = GetComponent<RectTransform>().sizeDelta;
+        Vector3 ownScale = GetComponent<RectTransform>().localScale;
 
         transform.SetParent(currCenter.transform);
-        float newPosX = -currSize.transform.localScale.x / 2 - ownSize.x / 2 - 5;
-        float newPosY = -currSize.transform.localScale.y / 2 + ownSize.y / 2;
+        float newPosX = currSize.transform.localScale.x / 2 + ownSize.x / 2 * ownScale.x;
+        float newPosY = -currSize.transform.localScale.y / 2;
         transform.localPosition = new Vector3(newPosX, newPosY, 0);
-        transform.eulerAngles = currCenter.transform.eulerAngles;
+        transform.localEulerAngles = new Vector3(0, 180, 0);
     }
 
     public void ToggleClippingBox() {
